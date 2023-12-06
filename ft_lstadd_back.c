d229149eece2cd5b6af92be6550a5389703b6600
Markus Weiss <maweiss@c4b4c6.42berlin.de>
2023-12-06 13:24:29 +0100

    ft_lst_back.c done and added to main_bonus.c with a basic test

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,19 +6,21 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 11:49:37 by maweiss           #+#    #+#             */
-/*   Updated: 2023/12/06 13:02:22 by maweiss          ###   ########.fr       */
+/*   Updated: 2023/12/06 13:09:31 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new);
-// {
-// 	t_list *tmp;
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *tmp;
 
-// 	tmp = *lst;
-// 	while (*tmp->next != NULL)
-// 	{
-// 		tmp = tmp->next;
-// 	}
-// }
+	tmp = *lst;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+	new->next = NULL;
+}
--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:14:07 by maweiss           #+#    #+#             */
-/*   Updated: 2023/12/06 12:57:28 by maweiss          ###   ########.fr       */
+/*   Updated: 2023/12/06 13:22:48 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ int	main(void)
 	t_list	*test;
 	t_list	*test2;
 	t_list	*test3;
+	t_list	*test4;
+	t_list	**signpost;
 	char	c;
 	int		a;
 
 	test = malloc(sizeof(t_list));
 	test2 = malloc(sizeof(t_list));
 	test3 = malloc(sizeof(t_list));
+	test4 = malloc(sizeof(t_list));
 	c = 'T';
 	a = 132654;
 	test->content = &c;
@@ -31,16 +34,32 @@ int	main(void)
 	test2->next = test3;
 	test3->content = &c;
 	test3->next = NULL;
+	signpost = &test;
+	test4->content = &a;
+	test4->next = NULL;
 	printf("ft_lstsize------------------------------------------\n");
 	printf("ft_lstsize test 1:\n");
 	if (ft_lstsize(test) == 3)
-		printf("ft_lstsize return value: 3 --> Correct\n");
+		printf("---> ft_lstsize return value: 3 --> Correct\n");
+	else if (ft_lstsize(test) != 3)
+		printf("---> ft_lstsize return value != 3 --> Fail!\n");
 	else
-		printf("ft_lstsize return value != 3 --> Fail!\n");
+		printf("---> Error!\n");
 	printf("ft_lstlast------------------------------------------\n");
 	printf("ft_lstlast test 1: \n");
 	if (ft_lstlast(test) == test3)
-		printf("ft_lstsize return value == test3 --> Correct\n");
-	if (ft_lstlast(test) != test3)
-		printf("ft_lstsize return value != test3 --> Fail\n");
+		printf("---> ft_lstlast return value == test3 --> Correct\n");
+	else if (ft_lstlast(test) != test3)
+		printf("---> ft_lstlast return value != test3 --> Fail\n");
+	else
+		printf("---> Error!\n");
+	printf("ft_lstadd_back------------------------------------------\n");
+	printf("ft_lstadd_back test 1: \n");
+	ft_lstadd_back(signpost, test4);
+	if (ft_lstsize(test) == 4)
+		printf("---> ft_lstsize return value: 4 --> Correct\n");
+	else if (ft_lstsize(test) != 4)
+		printf("---> ft_lstsize return value != 4 --> Fail!\n");
+	else
+		printf("---> Error!\n");
 }
